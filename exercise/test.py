c5f7092d209b70dfc0a5a15c39b231c921cb5e9a
echo001 <3049455827@qq.com>
2021-08-20 17:03:21 +0800

    update

--- exercise/test.py
+++ exercise/test.py
@@ -6,16 +6,25 @@ from io import BytesIO
 #content > section > div.container > table > tbody > tr:nth-child(5) > td:nth-child(1)
 
 url = 'https://proxy.ip3366.net/free/?action=china&page=1'
+url2 = 'http://www.cip.cc/'
 
-s = requests.session()
-s.keep_alive = False    #防止http连接过多
-response = requests.get(url)
-content = response.content
+def url_request(url):
+    s = requests.session()
+    s.keep_alive = False    #防止http连接过多
+    response = requests.get(url)
+    return response
 
-parser = etree.HTMLParser()
-content = etree.parse(BytesIO(content), parser=parser)
-doc = dict()
-for ip in content.findall('//td[1]'):
-    IP = ip.text
-for port in content.findall('//td[2]'):
-    PORT = port.text
+def ip_port(url):
+    response = url_request(url)
+    content = response.content
+    parser = etree.HTMLParser()
+    content = etree.parse(BytesIO(content), parser=parser)
+    doc = dict()
+    for ip in content.findall('//td[1]'):
+        IP = ip.text
+        print(IP)
+    for port in content.findall('//td[2]'):
+        PORT = port.text
+
+if __name__ == '__main__':
+    data = ip_port(url)
